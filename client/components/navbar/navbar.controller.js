18a6df4daa0c9f28aa79e61f6be448da4ff353d6
Ubuntu <kariudo@karbuntu.kariudo.b1.internal.cloudapp.net>
2014-08-04 16:07:48 +0000

    initial commit

---
+++ client/components/navbar/navbar.controller.js
@@ -0,0 +1,23 @@
+'use strict';
+/*global angular*/
+angular.module('animuterApp')
+  .controller('NavbarCtrl', function ($scope, $location, Auth) {
+    $scope.menu = [{
+      'title': 'Watchlist',
+      'link': '/'
+    }];
+
+    $scope.isCollapsed = true;
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+    $scope.getCurrentUser = Auth.getCurrentUser;
+
+    $scope.logout = function() {
+      Auth.logout();
+      $location.path('/login');
+    };
+
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
