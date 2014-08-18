593aefea239196546f62eeb7ceebb06bd8b84eea
Hunter Horsman <kariudo@gmail.com>
2014-08-18 14:29:18 +0000

    Fixed repeat animation for show list

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,8 @@ angular.module('animuterApp', [
   'ngSanitize',
   'btford.socket-io',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ngAnimate'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
